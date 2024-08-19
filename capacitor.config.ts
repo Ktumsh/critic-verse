cf0a14754ca6dca1f72557eb12235dd79c091bf9
Ktumsh <jbz.january@gmail.com>
2024-08-18 23:35:22 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'critic-verse',
+  webDir: 'www'
+};
+
+export default config;
